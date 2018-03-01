60e28ba9f0e5c8c8a4904cecc18f0a8f216980b3
Nick Muller <muller_nicky@hotmail.com>
2018-03-01 11:48:47 +0100

    Added repro. 'dotnet test' fails now.

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,14 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper.EquivalencyExpression;
 using Xunit;
 
 namespace AutoMapperCollectionRepro77
 {
-    public class UnitTest1
+    public class AutoMapperCollectionTests
     {
         [Fact]
-        public void Test1()
+        public async Task ConfigShouldBeThreadSafe()
         {
+            Action act = () =>
+            {
+                new AutoMapper.MapperConfiguration(cfg =>
+                {
+                    cfg.AddCollectionMappers();
+                });
+            };
+            var tasks = new List<Task>();
+            for (var i = 0; i < 5; i++)
+            {
+                tasks.Add(Task.Run(act));
+            }
 
+            await Task.WhenAll(tasks.ToArray());
         }
     }
 }
